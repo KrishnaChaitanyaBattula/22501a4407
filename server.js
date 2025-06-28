490232e5a3c23f3c0588104cf6556a380e7d018b
KrishnaChaitanyaBattula <battulachaitanya8899@gmail.com>
2025-06-28 12:02:00 +0530

    added logging middleware

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const http = require('http');
 const urlRoute = require('./route/urlRoute');
-require('./cron_job/cleanupJob'); 
+require('./cron_job/cleanupJob');
+const logger = require('./middleware/logger');
+
+function applyMiddleware(req, res, middlewares, handler) {
+  let idx = 0;
+  function next() {
+    if (idx < middlewares.length) {
+      middlewares[idx++](req, res, next);
+    } else {
+      handler(req, res);
+    }
+  }
+  next();
+}
 
 const server = http.createServer((req, res) => {
-  urlRoute.handle(req, res);
+  applyMiddleware(req, res, [logger], urlRoute.handle);
 });
 
-server.listen(process.env.PORT, () => {
+server.listen(3000, () => {
   console.log('URL shortener running on port 3000');
 });
